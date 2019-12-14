05ecbb22a1a460b20c0829ddf82db15457154fff
CodeInDna <ykarelia323@gmail.com>
2019-12-14 22:54:13 +0530

    Classification

---
+++ 13_Supervised_Learning_With_ScikitLearn/01_Classification.py
@@ -0,0 +1,54 @@
+# Supervised_Learning
+# What is Machine Learning ?
+# The art and science of:
+	# Giving computers the ability to learn to make decisions from data
+	# Without being explicitly programmed !
+# Examples:
+	# Learning to predict whether an email is spam or not
+	# Clustering wikipedia entries into different categories
+# Supervised Learning: Uses Labelled Data
+# Unsupervised Learning: Uses UnLabelled Data
+# 
+# Unsupervised learning: Uncovering hidden patterns from unlabeled data
+# Example:
+	# Grouping customers into distinct categories (Clustering)
+#
+# Reinforcement Learning
+# Software Agents interact with an environment
+	# Learn how to optimize their behavior
+	# Given a system of rewards and punishments
+	# Draws inspiration from behavioral psychology
+# Applications
+	# Economics
+	# Genetics
+	# Game Playing
+# AlphaGo: First Computer to Defeat the World Champion in Go
+
+########### Supervised Learning ###########
+# Predictor Variables/features and a target variable.
+# Aim: Predict the target variable, given the predictor variables.
+	# Classfication: Target Variable Consists of Categories
+	# Regression: Target variable is Continuous
+
+# NAMING CONVENTIONS
+# Features = Predictor Variables = Independent Variables
+# Target Variable = Dependent Variable = Response Variable
+
+# Automate time-consuming or expensive manual tasks
+	# Example: Doctor's Diagnosis
+
+# Make Predictions about the future
+	# Example: Will a customer click on an ad or not?
+
+# Need Labeled Data
+	# Historical data with labels
+	# Experiments to get labeled data
+	# Crowd-Sourcing Labeled Data
+# Which of these is a classification problem?
+# Once you decide to leverage supervised machine learning to solve a new problem, you need to identify whether your problem is better suited to classification or regression. This exercise will help you develop your intuition for distinguishing between the two.
+
+# Provided below are 4 example applications of machine learning. Which of them is a supervised classification problem?
+# Using labeled financial data to predict whether the value of a stock will go up or go down next week. <-
+# Using labeled housing price data to predict the price of a new house based on various features.
+# Using unlabeled data to cluster the students of an online education company into different categories based on their learning styles.
+# Using labeled financial data to predict what the value of a stock will be next week.
