c393aabddc451c37beb3d571817921dbc3649b47
CodeInDna <ykarelia323@gmail.com>
2019-12-21 22:08:53 +0530

    Generalization Error

---
+++ 16_Machine Learning with Tree-Based Models in Python/04_Generalization_Error.py
@@ -0,0 +1,72 @@
+# Instantiate the model
+# In the following set of exercises, you'll diagnose the bias and variance problems of a regression tree. The regression tree you'll define in this exercise will be used to predict the mpg consumption of cars from the auto dataset using all available features.
+# We have already processed the data and loaded the features matrix X and the array y in your workspace. In addition, the DecisionTreeRegressor class was imported from sklearn.tree.
+
+# Import train_test_split from sklearn.model_selection
+from sklearn.model_selection import train_test_split
+
+# Set SEED for reproducibility
+SEED = 1
+
+# Split the data into 70% train and 30% test
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=SEED)
+
+# Instantiate a DecisionTreeRegressor dt
+dt = DecisionTreeRegressor(max_depth=4, min_samples_leaf=0.26, random_state=SEED)
+
+# Evaluate the 10-fold CV error
+# In this exercise, you'll evaluate the 10-fold CV Root Mean Squared Error (RMSE) achieved by the regression tree dt that you instantiated in the previous exercise.
+
+# In addition to dt, the training data including X_train and y_train are available in your workspace. We also imported cross_val_score from sklearn.model_selection.
+
+# Note that since cross_val_score has only the option of evaluating the negative MSEs, its output should be multiplied by negative one to obtain the MSEs.
+# Compute the array containing the 10-folds CV MSEs
+MSE_CV_scores = - cross_val_score(dt, X_train, y_train, cv=10, 
+                       scoring='neg_mean_squared_error',
+                       n_jobs=-1)
+
+# Compute the 10-folds CV RMSE
+RMSE_CV = (MSE_CV_scores.mean())**(0.5)
+
+# Print RMSE_CV
+print('CV RMSE: {:.2f}'.format(RMSE_CV))
+# <script.py> output:
+#     CV RMSE: 5.14
+
+# Evaluate the training error
+# You'll now evaluate the training set RMSE achieved by the regression tree dt that you instantiated in a previous exercise.
+
+# In addition to dt, X_train and y_train are available in your workspace.
+# Import mean_squared_error from sklearn.metrics as MSE
+from sklearn.metrics import mean_squared_error as MSE
+
+# Fit dt to the training set
+dt.fit(X_train,y_train)
+
+# Predict the labels of the training set
+y_pred_train = dt.predict(X_train)
+
+# Evaluate the training set RMSE of dt
+RMSE_train = (MSE(y_train, y_pred_train))**(0.5)
+
+# Print RMSE_train
+print('Train RMSE: {:.2f}'.format(RMSE_train))
+# <script.py> output:
+#     Train RMSE: 5.15
+ # Notice how the training error is roughly equal to the 10-folds CV error you obtained in the previous exercice.
+
+# High bias or high variance?
+# In this exercise you'll diagnose whether the regression tree dt you trained in the previous exercise suffers from a bias or a variance problem.
+
+# The training set RMSE (RMSE_train) and the CV RMSE (RMSE_CV) achieved by dt are available in your workspace. In addition, we have also loaded a variable called baseline_RMSE which corresponds to the root mean-squared error achieved by the regression-tree trained with the disp feature only (it is the RMSE achieved by the regression tree trained in chapter 1, lesson 3). Here baseline_RMSE serves as the baseline RMSE above which a model is considered to be underfitting and below which the model is considered 'good enough'.
+
+# Does dt suffer from a high bias or a high variance problem?
+# In [1]: RMSE_train
+# Out[1]: 5.15
+
+# In [2]: RMSE_CV
+# Out[2]: 5.14
+
+# In [3]: baseline_RMSE
+# Out[3]: 5.1
+# dt suffers from high bias because RMSE_CV ≈ RMSE_train and both scores are greater than baseline_RMSE.
