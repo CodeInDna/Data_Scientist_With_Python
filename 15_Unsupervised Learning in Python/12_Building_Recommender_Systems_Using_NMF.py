84d7235a088fb3b979324b906db4201caf888d53
CodeInDna <ykarelia323@gmail.com>
2019-12-21 22:04:08 +0530

    Building Recommder System

---
+++ 15_Unsupervised Learning in Python/12_Building_Recommender_Systems_Using_NMF.py
@@ -0,0 +1,25 @@
+# Which articles are similar to 'Cristiano Ronaldo'?
+# In the video, you learned how to use NMF features and the cosine similarity to find similar articles. Apply this to your NMF model for popular Wikipedia articles, by finding the articles most similar to the article about the footballer Cristiano Ronaldo. The NMF features you obtained earlier are available as nmf_features, while titles is a list of the article titles.
+# Perform the necessary imports
+import pandas as pd
+from sklearn.preprocessing import normalize
+
+# Normalize the NMF features: norm_features
+norm_features = normalize(nmf_features)
+
+# Create a DataFrame: df
+df = pd.DataFrame(norm_features, index=titles)
+
+# Select the row corresponding to 'Cristiano Ronaldo': article
+article = df.loc['Cristiano Ronaldo']
+
+# Compute the dot products: similarities
+similarities = df.dot(article)
+
+# Display those with the largest cosine similarity
+print(similarities.nlargest())
+# Cristiano Ronaldo                1.000000
+# Franck Ribéry                    0.999972
+# Radamel Falcao                   0.999942
+# Zlatan Ibrahimović               0.999942
+# France national football team    0.999923
